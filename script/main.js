7bd1e0675e631926749c9de7fb7bfebf63211527
RPNephilim <70135450+RPNephilim@users.noreply.github.com>
2022-07-16 00:32:12 +0530

    v0.2

---
+++ script/main.js
@@ -0,0 +1,8 @@
+function cartimg(){
+    document.getElementById('img1').style.display="none";
+    document.getElementById("img2").style.display="block";
+}
+function orgimg(){
+    document.getElementById('img1').style.display="block";
+    document.getElementById("img2").style.display="none";
+}
